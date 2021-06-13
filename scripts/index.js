d6db536e7ebe619f3b66f57aa51a4540630f43d0
Ekaterina Zenkova <e.i.zenkova@yandex.ru>
2021-06-13 17:02:05 +0300

    submit

--- scripts/index.js
+++ scripts/index.js
@@ -11,4 +11,26 @@ const togglePopupVisibility = function() {
 popupOpenButtonElement.addEventListener('click', togglePopupVisibility);
 popupCloseButtonElement.addEventListener('click', togglePopupVisibility);
 
+let formElement = popupElement.querySelector('.form');
+let nameInput  = popupElement.querySelector('.form__item_name');
+let aboutInput = popupElement.querySelector('.form__item_about');
+
+console.log(formElement);
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    const nameInputValue  = nameInput.value;
+    const aboutInputValue  = aboutInput.value;
+
+    const profileName = document.querySelector('.profile__name');
+    const profileAbout = document.querySelector('.profile__about');
+
+    profileName.textContent = `${nameInputValue}`;
+    profileAbout.textContent = `${aboutInputValue}`;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+popupSaveButtonElement.addEventListener('click', togglePopupVisibility);
 
