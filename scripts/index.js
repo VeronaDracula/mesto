3042d6531ce4352b41509039ac13ae515ebf9114
Ekaterina Zenkova <e.i.zenkova@yandex.ru>
2021-06-13 15:30:12 +0300

    popup close, popup open - js

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,12 @@ const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupSaveButtonElement = popupElement.querySelector('.popup__save');
+
+const togglePopupVisibility = function() {
+    popupElement.classList.toggle('popup_is-opened');
+}
+
+popupOpenButtonElement.addEventListener('click', togglePopupVisibility);
+popupCloseButtonElement.addEventListener('click', togglePopupVisibility);
+
+
