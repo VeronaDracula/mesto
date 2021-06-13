f57a0fe98fc55178ca32bd503b715a9b562cf8e8
Ekaterina Zenkova <e.i.zenkova@yandex.ru>
2021-06-13 14:11:41 +0300

    popup js

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+const popupSaveButtonElement = popupElement.querySelector('.popup__save');
