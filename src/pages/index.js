2163f6d1462f552aa1d4c7989e3f82fac247cce4
Ekaterina Zenkova <e.i.zenkova@yandex.ru>
2021-08-17 15:29:37 +0300

    webpack css, images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import './index.css';
+
 import {initialCards} from '../utils/constants.js'
 import {Card} from '../components/Card.js'
 import {Section} from '../components/Section.js'
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: { main: './src/pages/index.js' },
@@ -19,7 +20,19 @@ module.exports = {
                 test: /\.js$/,
                 use: 'babel-loader',
                 exclude: '/node_modules/'
-            }
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, {
+                    loader: 'css-loader',
+                    options: { importLoaders: 1 }
+                },
+                    'postcss-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/resource'
+            },
         ]
     },
     mode: 'development',
@@ -27,6 +40,7 @@ module.exports = {
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'src', 'index.html')
-        })
+        }),
+        new MiniCssExtractPlugin(),
     ]
 }
